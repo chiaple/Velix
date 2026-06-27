a3ee8792de390e738f7a545c5080d417c0d0085e
chiaple <chiaple.lei@icloud.com>
2026-06-28 00:21:49 +0800

    Add initial project files including GPIO, ADC, I2C, and DMA configurations

---
+++ software/Core/Inc/main.h
@@ -0,0 +1,93 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file           : main.h
+  * @brief          : Header for main.c file.
+  *                   This file contains the common defines of the application.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Define to prevent recursive inclusion -------------------------------------*/
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* Includes ------------------------------------------------------------------*/
+#include "stm32g4xx_hal.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+
+/* USER CODE END Includes */
+
+/* Exported types ------------------------------------------------------------*/
+/* USER CODE BEGIN ET */
+
+/* USER CODE END ET */
+
+/* Exported constants --------------------------------------------------------*/
+/* USER CODE BEGIN EC */
+
+/* USER CODE END EC */
+
+/* Exported macro ------------------------------------------------------------*/
+/* USER CODE BEGIN EM */
+
+/* USER CODE END EM */
+
+/* Exported functions prototypes ---------------------------------------------*/
+void Error_Handler(void);
+
+/* USER CODE BEGIN EFP */
+
+/* USER CODE END EFP */
+
+/* Private defines -----------------------------------------------------------*/
+#define key1_Pin GPIO_PIN_13
+#define key1_GPIO_Port GPIOC
+#define key2_Pin GPIO_PIN_14
+#define key2_GPIO_Port GPIOC
+#define Encoder_CS_Pin GPIO_PIN_4
+#define Encoder_CS_GPIO_Port GPIOA
+#define ST7789_DC_Pin GPIO_PIN_4
+#define ST7789_DC_GPIO_Port GPIOC
+#define ST7789_RST_Pin GPIO_PIN_5
+#define ST7789_RST_GPIO_Port GPIOC
+#define SP3485_Pin GPIO_PIN_12
+#define SP3485_GPIO_Port GPIOB
+#define ST7789_CS_Pin GPIO_PIN_6
+#define ST7789_CS_GPIO_Port GPIOC
+#define FRAM_WP_Pin GPIO_PIN_8
+#define FRAM_WP_GPIO_Port GPIOC
+#define LED_R_Pin GPIO_PIN_12
+#define LED_R_GPIO_Port GPIOC
+#define LED_G_Pin GPIO_PIN_2
+#define LED_G_GPIO_Port GPIOD
+#define LED_B_Pin GPIO_PIN_3
+#define LED_B_GPIO_Port GPIOB
+#define encoder_Pin GPIO_PIN_4
+#define encoder_GPIO_Port GPIOB
+
+/* USER CODE BEGIN Private defines */
+
+/* USER CODE END Private defines */
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
